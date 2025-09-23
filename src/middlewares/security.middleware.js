bcf50ee8f00bd195fc98eba01b3e050deb60453b
Harshit <hktiwari980@gmail.com>
2025-09-23 05:54:29 +0530

    fixed2

--- src/middlewares/security.middleware.js
+++ src/middlewares/security.middleware.js
@@ -4,11 +4,20 @@ const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 
 const corsOptions = {
-  origin: [
-    'http://localhost:3000', 
-    'http://localhost:3001',
-    'https://blogging-web-app-full-stack-project.vercel.app'
-  ],
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:3001', 
+      'https://blogging-web-app-full-stack-project.vercel.app',
+      process.env.CLIENT_URL
+    ].filter(Boolean);
+    
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
